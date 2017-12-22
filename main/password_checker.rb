654328faa3e31038c2739eb7a528b4700eb6cc28
zerotani <asuka@ohtani.gr.jp>
2017-12-22 12:31:38 +0000

    Split constants to yml

--- main/password_checker.rb
+++ main/password_checker.rb
@@ -1,22 +1,11 @@
+require "yaml"
 =begin
 Usage
 $> ruby main.rb "password_string"
 =end
 class PasswordChecker
   attr_reader :password_string
-  VALID_MES = "パスワードがだめっぽいです。"
-  VALID_RULE = {
-    number_include: { reg: /^(([^\d])+|)$/, mes: "数字が含まれていません" },
-    lower_include: { reg: /^(([^a-z])+|)$/, mes: "小文字が含まれていません" },
-    upper_include: { reg: /^(([^A-Z])+|)$/, mes: "大文字が含まれていません" },
-    enough_length: { reg: /^(.{0,7})$/, mes: "既定の文字数に達していません" },
-  }.freeze
-  OPTION_MES = "よさそうなパスワードです！ よきかな"
-  OPTION_RULE = {
-    no_special: { reg: /.*/, mes: "" },
-    valid_symbol: { reg: /[!"#$%&'\(\)*+,-.\/\\:;<=>?@\[\]^_`{|}~]+/, mes: "記号が含まれています！ Good" },
-    start_with_symbol: { reg: /\A[^\w]/, mes: "記号から始まっています！ Great！" },
-  }.freeze
+  SETTING = YAML.load_file(File.join(File.dirname(__FILE__), "..", "config", "settings.yml")).freeze
 
   def initialize(password_string)
     @password_string = password_string
@@ -27,14 +16,32 @@ class PasswordChecker
   end
 
   def check
-    checker(VALID_MES, VALID_RULE) || checker(OPTION_MES, OPTION_RULE)
+    checker(valid_message, valid_rules) || checker(option_message, option_rules)
   end
 
   def checker(mes, rules)
     array = []
     rules.each do |key, rule|
-      array << rule[:mes] if password_string.match(rule[:reg])
+      array << rule["mes"] if password_string.match(rule["reg"])
     end
     array[0] ? array.reject(&:empty?).unshift(mes).join("\n") : nil
+  end
+
+  private
+
+  def valid_message
+    SETTING["valid"]["first_message"]
+  end
+
+  def valid_rules
+    SETTING["valid"]["rules"]
+  end
+
+  def option_message
+    SETTING["option"]["first_message"]
+  end
+
+  def option_rules
+    SETTING["option"]["rules"]
   end
 end
