1bb7b4e19c3a843db676d0e02ac2429ede49015a
zerotani <asuka@ohtani.gr.jp>
2017-12-22 10:45:40 +0000

    Revert "Refactoring"
    This reverts commit cfb226ca623dde1e7216535c054216687d8666ae.

--- main.rb
+++ main.rb
@@ -7,14 +7,13 @@ $> ruby main.rb "password_string"
 =end
 class PasswordChecker
   attr_reader :password_string
-  VALID_MES = "パスワードがだめっぽいです。\n".freeze
   VALID_RULE = {
     number_include: { reg: /\d+/, mes: "数字が含まれていません\n" },
     lower_include: { reg: /[a-z]+/, mes: "小文字が含まれていません\n" },
     upper_include: { reg: /[A-Z]+/, mes: "大文字が含まれていません\n" },
     enough_length: { reg: /.{8,}/, mes: "既定の文字数に達していません\n" },
   }.freeze
-  OPTION_MES = "よさそうなパスワードです！ よきかな\n".freeze
+
   OPTION_RULE = {
     valid_symbol: { reg: /[!"#$%&'\(\)*+,-.\/\\:;<=>?@\[\]^_`{|}~]+/, mes: "記号が含まれています！ Good\n" },
     start_with_symbol: { reg: /\A[^\w]/, mes: "記号から始まっています！ Great！\n" },
@@ -29,15 +28,30 @@ class PasswordChecker
   end
 
   def check
-    error_messages = checker(VALID_RULE)
-    return VALID_MES + error_messages if error_messages.present?
-    oneup_messages = checker(OPTION_RULE)
-    OPTION_MES + oneup_messages
+    errors = []
+    oneup = []
+
+    VALID_RULE.each do |key, rule|
+      errors << key unless password_string =~ rule[:reg]
+    end
+    return error_messages(errors) if errors[0]
+
+    OPTION_RULE.each do |key, rule|
+      oneup << key if password_string =~ rule[:reg]
+    end
+    return success_messages(oneup)
   end
 
-  def checker(rule)
-    rule.each.with_object("") do |key, mes|
-      mes << rule[key][:mes]
+  def error_messages(errors)
+    result = ""
+    errors.each.with_object("パスワードがだめっぽいです。\n") do |key, mes|
+      mes << VALID_RULE[key][:mes]
+    end
+  end
+
+  def success_messages(oneup)
+    oneup.each.with_object("よさそうなパスワードです！ よきかな\n") do |key, mes|
+      mes << OPTION_RULE[key][:mes]
     end
   end
 end
